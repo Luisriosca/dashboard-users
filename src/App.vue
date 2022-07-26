f6ab22dfee655f7e84acc9db0dc4d8a8b75e4671
Luisriosca <luis.rios.9815@gmail.com>
2022-07-26 15:16:42 -0500

    Removing logs

--- src/App.vue
+++ src/App.vue
@@ -43,20 +43,7 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>Fake User Dashboard</v-app-bar-title>
-
       <v-spacer></v-spacer>
-<!-- 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn> -->
     </v-app-bar>
 
       <v-toolbar-title>Fake UserDashboard</v-toolbar-title>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h1>Users Dashboard</h1>
     <v-container fluid>
       <v-row align="center">
-        <v-col cols="12" sm="1" class="py-2 d-flex justify-content-center">
+        <v-col cols="12" sm="2" class="py-2 d-flex justify-content-center">
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
               <v-icon color="primary" dark v-bind="attrs" v-on="on" >
@@ -13,7 +13,7 @@
             <span>Select a gender filter</span>
           </v-tooltip>
         </v-col>
-        <v-col cols="12" sm="1" class="py-2">
+        <v-col cols="12" sm="2" class="py-2 d-flex justify-content-center">
     
           <v-btn-toggle v-model="toggleGenderFilter">
             <v-btn @click="filterGender('female')">
@@ -29,6 +29,17 @@
             </v-btn>
     
           </v-btn-toggle>
+        </v-col>
+        
+        <v-col cols="12" sm="2" class="py-2 d-flex justify-content-center">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon color="primary" dark v-bind="attrs" v-on="on" >
+                mdi-human-cane
+              </v-icon>
+            </template>
+            <span>Select for an age range</span>
+          </v-tooltip>
         </v-col>
 
       </v-row>
