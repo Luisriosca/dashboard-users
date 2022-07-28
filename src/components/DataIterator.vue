3f1e5d093401408f6c8f37091f87b3bf297ba36d
Luisriosca <luis.rios.9815@gmail.com>
2022-07-27 23:59:08 -0500

    New iterator component

---
+++ src/components/DataIterator.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-data-iterator :items="dataIterator1">
+        <template v-slot:default="props">
+            <v-row>
+                <v-col v-for="item in props.items" :key="item.id.value" cols="12" sm="6" md="4" lg="3" >
+                    <v-card>
+                        <v-list class="d-flex justify-content-center">
+                            <v-list-item-avatar :size="62"> 
+                                <v-img
+                                    :alt="`${item.name.first} avatar`"
+                                    :src="item.picture.thumbnail"
+                                ></v-img>
+                            </v-list-item-avatar>
+                        </v-list>
+
+                        <v-list class="justify-center" dense>
+                            <v-list-item>
+                                <v-list-item-content class="justify-center">
+                                {{ item.name.first  + ' ' +  item.name.last}}
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+
+                        <v-list class="justify-center" dense>
+                            <v-list-item>
+                                <v-list-item-content class="justify-center">
+                                {{item.email}}
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+
+                    </v-card>
+                </v-col>
+            </v-row>
+
+        </template>
+    </v-data-iterator>
+</template>
+
+<script>
+    
+
+</script>
